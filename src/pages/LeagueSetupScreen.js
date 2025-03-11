7406f9804ba564579908d76c986bb98bfd4ed97f
Raj <raj.nair@colorado.edu>
2025-03-12 02:15:18 +0530

    Updating League Size

--- src/pages/LeagueSetupScreen.js
+++ src/pages/LeagueSetupScreen.js
@@ -212,7 +212,7 @@ const LeagueSetupScreen = ({onLeagueChosen}) => {
 
       }else{
 
-        if (participantParam.length >=3 ) {
+        if (participantParam.length >=8 ) {
           lockValue = "draft";
         }else{
           lockValue = "predraft";
